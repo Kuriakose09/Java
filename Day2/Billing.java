f1c099de1464d831bf92575f5df228475af064dc
Kuriakose Antony <kuriakoseantony09@gmail.com>
2025-10-19 23:21:58 +0530

    Day2 Home work

---
+++ Day2/Billing.java
@@ -0,0 +1,13 @@
+
+public class Billing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String item = "Soap";
+		int Quantity = 4;
+		double price = 18.75;
+		double tot_price = Quantity * price;
+		System.out.println("Item name:"+item+", Quantity:"+Quantity+", Unit Price:"+price+", Total cost:"+tot_price);
+	}
+
+}
