657842e13262ac77701efacda9a0243e7d8cf5ab
Kuriakose Antony <kuriakoseantony09@gmail.com>
2025-11-05 21:05:22 +0530

    Machine Test 2

---
+++ MachineTest/Second.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Second{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        int num = input.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(num+" * "+i+" = "+num*i);
+        }
+    }
+}
